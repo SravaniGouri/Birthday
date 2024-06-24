d07075a691ffb321883d415f143f14c8e6fcf3f5
SravaniGouri <pavanvasam777@gmail.com>
2024-06-24 19:56:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
   
     $(".tarot-card").click(function () {
       var card = $(this);
-      if (count < 28) {
+      if (count < 29) {
         $(this).removeClass("top").addClass("bottom");
   
         count++;
       }
-      if (count == 27) {
+      if (count == 28) {
         $("div").removeClass("bottom");
   
         count = 0;
